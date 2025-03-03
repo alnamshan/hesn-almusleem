5d571b1f4b2a0f34968bffe7cf0ee81681b1a4fb
alnamshan <mh_om2003@hotmail.com>
2025-03-03 03:36:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+﻿self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('hisn-muslim-cache').then((cache) => {
+            return cache.addAll([
+                './index.html',
+                './manifest.json',
+                './icon.png',
+                './audio1.mp3',
+                './audio2.mp3',
+                './audio3.mp3'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
